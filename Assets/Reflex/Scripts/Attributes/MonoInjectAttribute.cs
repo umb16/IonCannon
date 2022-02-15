67b814d07ce5eb0037aeebf68f51c83654a399cd
Mark Kashchaev <unihilist@ya.ru>
2022-02-15 19:22:03 +0300

    Удалил рефлекс

--- Assets/Reflex/Scripts/Attributes/MonoInjectAttribute.cs
+++
@@ -1,7 +0,0 @@
-﻿using System;
-
-namespace Reflex.Scripts.Attributes
-{
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Method)]
-    public class MonoInjectAttribute : Attribute { }
-}
--- Assets/Reflex/Scripts/Resolvers/Resolver.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace Reflex
-{
-	internal abstract class Resolver
-	{
-		internal abstract object Resolve(Type contract, Container container);
-	}
-}
