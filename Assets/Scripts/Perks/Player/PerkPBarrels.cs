4da9d1860f7e68b0d159a1a201010e5864d08a26
Mark Kashchaev <unihilist@ya.ru>
2022-04-07 22:05:44 +0300

    Поправил спавн ящиков

--- Assets/Scripts/Perks/Player/PerkPBarrels.cs
+++ Assets/Scripts/Perks/Player/PerkPBarrels.cs
@@ -50,7 +50,7 @@ public class PerkPBarrels : IPerk
     private async UniTask CreateBarrel()
     {
         _lastSpawnTime = Time.time;
-        var go = await PrefabCreator.Instantiate("Obj_Barrel", new Vector3(Random.value * 2 - 1, Random.value * 2 - 1).normalized * 30);
+        var go = await PrefabCreator.Instantiate("Obj_Barrel", new Vector3(Random.value * 2 - 1, Random.value * 2 - 1).normalized * 30 * Random.value);
         _mob.AllMobs.Add(go.GetComponent<IMob>());
     }
 
